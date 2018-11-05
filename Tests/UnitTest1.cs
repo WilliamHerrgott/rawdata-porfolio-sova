d26c3c2aa11635458dfaf0b0e67b13a34d288f2b
Boris Le Meec <boris.le-meec@epitech.eu>
2018-11-05 18:50:22 +0100

    yes

---
+++ Tests/UnitTest1.cs
@@ -0,0 +1,21 @@
+
+using StackOverflowData;
+using WebService.Controllers;
+using Xunit;
+
+namespace Tests
+{
+    public class WebServiceTests
+    {
+        [Fact]
+        public void Create_User_Successful() {
+            var service = new DataService();
+            var u1 = service.GetUser("test");
+//            if (u1 != null)
+//                service.DeleteUser(u1.Id);
+//            var user = service.CreateUser("test@test.te", "test", "Roskilde", "testpwd", "salt");
+//            Assert.NotEqual(null, user);
+        }
+    }
+    
+}
