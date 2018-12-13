1a25804b7521bf18ac86d30a4f53be6f7f123e71
William Herrgott <herrgott.william@gmail.com>
2018-12-13 14:41:22 +0100

    Context with real db informations

--- StackOverflowData/StackOverflowContext.cs
+++ StackOverflowData/StackOverflowContext.cs
@@ -35,7 +35,7 @@ namespace StackOverflowData {
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
             base.OnConfiguring(optionsBuilder);
-            optionsBuilder.UseNpgsql("host=localhost;db=stackoverflow;uid=postgres;pwd=");
+            optionsBuilder.UseNpgsql("host=rawdata.ruc.dk;db=raw9;uid=raw9;pwd=oXa+yMeV");
             // you only need this if you want to see the SQL statments created
             // by EF
             optionsBuilder.UseLoggerFactory(MyLoggerFactory)
